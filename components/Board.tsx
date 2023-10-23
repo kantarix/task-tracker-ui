ad9612615cfbefba9b01c3031968ff718a6e047f
Flethy <petrova.vera.ev@gmail.com>
2023-10-23 10:37:03 +0300

    state saving for columns

--- components/Board.tsx
+++ components/Board.tsx
@@ -6,9 +6,10 @@ import { DragDropContext, DropResult, Droppable } from 'react-beautiful-dnd';
 import Column from './Column';
 
 function Board() {
-  const [board, getBoard] = useBoardStore((state) => [
+  const [board, getBoard, setBoardState] = useBoardStore((state) => [
     state.board,
     state.getBoard,
+    state.setBoardState,
   ]);
 
   useEffect(() => {
@@ -16,7 +17,20 @@ function Board() {
   }, [getBoard])
 
   const handleOnDragEnd = (result: DropResult) => {
-    
+    const { destination, source, type } = result;
+
+    if (!destination) return;
+
+    if(type === "column") {
+      const entries = Array.from(board.columns.entries());
+      const [removed] = entries.splice(source.index, 1);
+      entries.splice(destination.index, 0, removed);
+      const rearrangedColumns = new Map(entries);
+      setBoardState({
+        ...board, 
+        columns: rearrangedColumns,
+      })
+    }
   };
 
   return (
